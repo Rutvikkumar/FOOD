e57407074b37b431d37d13870e76e83049a7a78f
Rutvik Parmar <parmarrutvik1414@gmail.com>
2024-05-03 08:08:40 +0530

    hello

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,11 @@
-const express = require('express');
+const express = require("express");
 const users = express();
 const create = require("../controller/userCtr");
 
-
 const router = express.Router();
 
-
-router.get('/',create.Register);
-router.post('/',create.createUser);
-router.get('/',create.verifyMail)
-
-
+router.get("/", create.Register);
+router.post("/", create.createUser);
+router.get("/", create.verifyMail);
 
 module.exports = router;
-
-
